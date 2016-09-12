357e5782ac43786bd54d4ef9fa849d1251fb3a87
Leo Dearden <leo@leodearden.org>
2016-09-12 18:10:56 +0100

    Replace relative with absolute paths in build.py to allow building from other directories.

--- pattern/build.py
+++ pattern/build.py
@@ -1,12 +1,23 @@
+#!/usr/bin/python
+
 import os, glob, re
 from fabricate import *
+import inspect
 
-setup(dirs=['.', '../FastLED'])
+base_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
 
-classes = [re.sub(r'\.cpp', '', name) for name in glob.glob('*.cpp')] + ['../FastLED/hsv2rgb']
-cflags = '-DPLATFORM_DUMMY=1 -I. -I/opt/X11/include -I../FastLED -I../CImg -I../pattern/Catch/single_include -O0 -g3 -Wall'.split()
+def script_relative(path):
+    return os.path.abspath(os.path.join(base_dir, path))
+
+setup(dirs=[base_dir, script_relative('../FastLED')])
+
+classes = [re.sub(r'\.cpp', '', name) for name in glob.glob(script_relative('*.cpp'))] + [script_relative('../FastLED/hsv2rgb')]
+cflags = '-DPLATFORM_DUMMY=1 -I/opt/X11/include -O0 -g3 -Wall'.split() + [
+        '-I' + script_relative(inc)
+        for inc in ['.', '../FastLED', '../CImg', '../pattern/Catch/single_include']
+    ]
 ldflags = '-L/opt/X11/lib -lX11'.split()
-build_dir = 'build'
+build_dir = script_relative('build')
 
 def preview():
     build_and_run('mains/preview', 'preview')
@@ -19,7 +30,7 @@ def build_and_run(main, name):
     shell(oname(build_dir, name), silent=False)
 
 def build(main, name):
-    sources = classes + [main]
+    sources = classes + [script_relative(main)]
     objects = compile(sources)
     link(name, objects)
 
