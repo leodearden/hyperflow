65542b0f440677fd11cbc81ce7223bf856bad8c6
Leo Dearden <leo@leodearden.org>
2016-10-08 12:21:28 +0100

    Speed optimise for Teensy (precompute sines).

--- pattern/PrimeCycles.cpp
+++ pattern/PrimeCycles.cpp
@@ -13,22 +13,43 @@ float sinusoid(int i) {
 	return sin(2 * M_PI * (i % period) / period);
 }
 
+template <int period>
+class FastSin {
+public:
+	FastSin() {
+		for (int i = 0; i < period; ++i) {
+			lut[i] = sin(2 * M_PI * (i % period) / period);
+		}
+	}
+	float operator()(int i) {return lut[i%period];}
+private:
+	float lut[period];
+};
+
 void PrimeCycles::updateFrame(CHSV* frame) {
 	++frame_count;
 	float saturation_sin = ::sinusoid<2791>(frame_count);
 	float saturation = 255 - 210 * saturation_sin * saturation_sin;
 	float base_colour = ::sinusoid<20071>(frame_count);
+	static FastSin<587> fs587;
+	static FastSin<593> fs593;
+	static FastSin<331> fs331;
+	static FastSin<349> fs349;
+	static FastSin<131> fs131;
+	static FastSin<137> fs137;
+	static FastSin<443> fs443;
+	static FastSin<449> fs449;
 	for (int i = 0; i < Pattern::num_leds; ++i) {
-		float value_sin = (  ::sinusoid<587>(frame_count + i)
-				            + ::sinusoid<593>(frame_count + i)) / 2
-				         * (  ::sinusoid<331>(frame_count + Pattern::num_leds - i)
-						    + ::sinusoid<349>(frame_count + Pattern::num_leds - i))
+		float value_sin = (  fs587(frame_count + i)
+				            + fs593(frame_count + i)) / 2
+				         * (  fs331(frame_count + Pattern::num_leds - i)
+						    + fs349(frame_count + Pattern::num_leds - i))
 				         / 2;
 		frame[i] = CHSV(
-				255 * ( ( ::sinusoid<131>(frame_count + i)
-			            + ::sinusoid<137>(frame_count + i)) /2
-				      * ( ::sinusoid<443>(frame_count + Pattern::num_leds - i)
-					    + ::sinusoid<449>(frame_count + Pattern::num_leds - i)) / 2
+				255 * ( ( fs131(frame_count + i)
+			            + fs137(frame_count + i)) /2
+				      * ( fs443(frame_count + Pattern::num_leds - i)
+					    + fs449(frame_count + Pattern::num_leds - i)) / 2
 						+ base_colour),
 				saturation,
 				(128 + 127 * value_sin) / (1 + (255 - saturation) / 768)
